42453ac2e12e789013192b42e5462888a6462e44
Shannon Rothe <shannon.michael.rothe@gmail.com>
2023-11-05 08:01:20 +1100

    404

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,35 @@
 // Uncomment this block to pass the first stage
+use anyhow::Context;
 use std::{
     io::{BufRead, Read, Write},
     net::TcpListener,
 };
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buffer = Vec::new();
-                println!("{buffer:?}");
+        let mut stream = stream?;
 
-                if let Err(e) = stream.read_to_end(&mut buffer) {
-                    eprintln!("failed to read: {}", e);
-                }
+        let mut buf = [0; 1024];
+        stream.read(&mut buf)?;
 
-                let lines: Vec<_> = buffer.lines().flatten().collect();
-                println!("{:?}", lines);
-                let first_line = lines.first().unwrap();
-                let mut parts = first_line.split_whitespace();
-                match parts.nth(1) {
-                    Some("/") => stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap(),
-                    _ => stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap(),
-                }
+        let lines: Vec<_> = buf.lines().flatten().collect();
+        let first_line = lines.first().unwrap();
+        let mut parts = first_line.split_whitespace();
+        match parts.nth(1) {
+            Some("/") => {
+                stream
+                    .write_all(b"HTTP/1.1 200 OK\r\n\r\n")
+                    .context("failed to write 200")?;
             }
-            Err(e) => {
-                println!("error: {}", e);
+            _ => {
+                stream
+                    .write_all(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                    .context("failed to write 404")?;
             }
         }
     }
+
+    Ok(())
 }
