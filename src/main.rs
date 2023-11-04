32bbd521442e93d08368b1e654f7c1b60874ae99
Shannon Rothe <shannon.michael.rothe@gmail.com>
2023-11-05 07:56:51 +1100

    404

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,23 @@ use std::{
 };
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                let mut buffer = [0; 1024];
-                if let Err(e) = stream.read_exact(&mut buffer) {
+                let mut buffer = Vec::new();
+                println!("{buffer:?}");
+
+                if let Err(e) = stream.read_to_end(&mut buffer) {
                     eprintln!("failed to read: {}", e);
                 }
 
                 let lines: Vec<_> = buffer.lines().flatten().collect();
+                println!("{:?}", lines);
                 let first_line = lines.first().unwrap();
                 let mut parts = first_line.split_whitespace();
-                let path = parts.nth(1);
-                match path {
+                match parts.nth(1) {
                     Some("/") => stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap(),
                     _ => stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap(),
                 }
