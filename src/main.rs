dcdbfd56fd4344a19de9b748b56b1c7d97a9f6e2
Shannon Rothe <shannon.michael.rothe@gmail.com>
2023-11-05 07:52:16 +1100

    404

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,7 +15,19 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                let mut buffer = [0; 1024];
+                if let Err(e) = stream.read_exact(&mut buffer) {
+                    eprintln!("failed to read: {}", e);
+                }
+
+                let lines: Vec<_> = buffer.lines().flatten().collect();
+                let first_line = lines.first().unwrap();
+                let mut parts = first_line.split_whitespace();
+                let path = parts.nth(1);
+                match path {
+                    Some("/") => stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap(),
+                    _ => stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap(),
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
